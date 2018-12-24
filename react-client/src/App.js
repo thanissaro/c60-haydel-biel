054bb7b97c01e4cfc2e6580b1c1b576e6e9daa34
Kevin Luang-orn <kluangorn@gmail.com>
2018-12-23 20:06:06 -0800

    react bootstrap installed

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Button, ButtonToolbar } from "react-bootstrap";
 
 const Index = () => <h2>Home</h2>;
 const About = () => <h2>About</h2>;
@@ -25,6 +26,28 @@ const AppRouter = () => (
       <Route path="/" exact component={Index} />
       <Route path="/about/" component={About} />
       <Route path="/users/" component={Users} />
+      <ButtonToolbar>
+  {/* Standard button */}
+  <Button>Default</Button>
+
+  {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+  <Button bsStyle="primary">Primary</Button>
+
+  {/* Indicates a successful or positive action */}
+  <Button bsStyle="success">Success</Button>
+
+  {/* Contextual button for informational alert messages */}
+  <Button bsStyle="info">Info</Button>
+
+  {/* Indicates caution should be taken with this action */}
+  <Button bsStyle="warning">Warning</Button>
+
+  {/* Indicates a dangerous or potentially negative action */}
+  <Button bsStyle="danger">Danger</Button>
+
+  {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
+  <Button bsStyle="link">Link</Button>
+</ButtonToolbar>;
     </div>
   </Router>
 );
