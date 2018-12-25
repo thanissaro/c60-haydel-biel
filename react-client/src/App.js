e6382fb61a3d9a21da96a0bfb2de26de6b6b0f77
Kevin Luang-orn <kluangorn@gmail.com>
2018-12-24 16:39:22 -0800

    wip

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 const Index = () => <h2>Home</h2>;
 const About = () => <h2>About</h2>;
 const Users = () => <h2>Users</h2>;
+const Team = () => <h2>Team</h2>;
 
 const AppRouter = () => (
   <Router>
@@ -18,6 +19,9 @@ const AppRouter = () => (
           </li>
           <li>
             <Link to="/users/">Users</Link>
+          </li>
+          <li>
+            <Link to="/team/">Team</Link>
           </li>
         </ul>
       </nav>
